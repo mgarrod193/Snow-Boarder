35d4e35ffb87e86ddacec7db402d078301885be0
mgarrod <mgarrod193@gmail.com>
2022-11-13 17:47:12 +0700

    added particles and scene reload

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 2f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag != "Finish Line")
         {
-            Debug.Log("You Died");
+            Invoke("ReloadScene", reloadDelay);
         }
 
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("You Finished");
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+
+    [SerializeField] float reloadDelay = 2f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            Debug.Log("You Finished!");
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {   
+        SceneManager.LoadScene(0);
+        Debug.Log("You Finished");
     }
 }
